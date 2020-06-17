499b8ece6b9755575ebde880ed552ea23627a620
Tumble <tumblegamer@gmail.com>
2020-06-17 11:24:13 +0100

    server setup

--- client.js
+++ client.js
@@ -1,58 +1,58 @@
 const
-	ZERO_VECTOR = {x:0,y:0}
-	DEBUG = true
+	ZERO_VECTOR = { x: 0, y: 0 }
+DEBUG = true
 {
 	var oldLog = console.log;
-	console.log = (...data)=>{
-		if(DEBUG) oldLog(...data);
+	console.log = (...data) => {
+		if (DEBUG) oldLog(...data);
 	}
 }
 
 var game;
 
-function lerpXY(a,b,t) {
+function lerpXY(a, b, t) {
 	return {
-		x:a.x+t*(b.x-a.x),
-		y:a.y+t*(b.y-a.y)
+		x: a.x + t * (b.x - a.x),
+		y: a.y + t * (b.y - a.y)
 	}
 }
 
-function generateCoins(n,bounds) {
+function generateCoins(n, bounds) {
 	var pos = [];
 	for (let i = 0; i < n; i++) {
-		pos.push(Math.random()*bounds.x);
-		pos.push(Math.random()*bounds.y);		
+		pos.push(Math.random() * bounds.x);
+		pos.push(Math.random() * bounds.y);
 	}
 	return pos;
 }
 
 class Component {
 	constructor(gameObject) {
-		this.gameObject = {worldX:0,worldY:0};
-		gameObject&&(this.gameObject = gameObject);
+		this.gameObject = { worldX: 0, worldY: 0 };
+		gameObject && (this.gameObject = gameObject);
 	}
 }
 
 class PhysicsBody extends Component {
 	constructor(gameObject) {
 		super(gameObject);
-		this.velocity = {x:0,y:0};
+		this.velocity = { x: 0, y: 0 };
 		this.dampening = .035;
 	}
 
 	update() {
 		this.gameObject.worldX += this.velocity.x;
 		this.gameObject.worldY += this.velocity.y;
-		this.velocity = lerpXY(this.velocity,ZERO_VECTOR,this.dampening);
+		this.velocity = lerpXY(this.velocity, ZERO_VECTOR, this.dampening);
 	}
-	addImpulse(x,y) {
+	addImpulse(x, y) {
 		this.velocity.x += x;
 		this.velocity.y += y;
 	}
 }
 
 class GameObject extends createjs.Shape {
-	constructor(game,x,y) {
+	constructor(game, x=0, y=0) {
 		super();
 		this.worldX = x;
 		this.worldY = y;
@@ -62,91 +62,102 @@ class GameObject extends createjs.Shape {
 	}
 
 	update() {
-		var pos = game.worldToLocalPos(this.worldX,this.worldY)
+		var pos = game.worldToLocalPos(this.worldX, this.worldY)
 		this.x = pos.x;
 		this.y = pos.y;
 	}
 
-	setPos(x,y) {
+	setPos(x, y) {
 		this.worldX = x;
 		this.worldY = y;
 	}
 }
 class Crosshair extends GameObject {
-	constructor(game,x,y) {
-		super(game,x,y);
-		this.graphics.beginFill("Green").drawCircle(0,0,20);
+	constructor(game, x, y) {
+		super(game, x, y);
+		this.graphics.beginFill("Green").drawCircle(0, 0, 20);
 	}
 }
 
-class Player extends GameObject {
-	constructor(game,x,y) {
-		super(game,x,y);
-		this.pair = "hello";
-		this.mode = 0;
-		this.graphics.beginFill("DeepSkyBlue").drawCircle(0,0,50);
+class Ship extends GameObject {
+	constructor(game,name) {
+		super(game);
+		this.name = name;
+		this.mode = -1;
+		this.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
 		this.speed = .3
-		this.moving = {up:0,down:0,left:0,right:0};
-		this.crosshairPos = {x:0,y:0};
+		this.moving = { up: 0, down: 0, left: 0, right: 0 };
+		this.crosshairPos = { x: 0, y: 0 };
 
 		//Child GameObjects
-		this.crosshair = new Crosshair(game,x,y);
+		this.crosshair = new Crosshair(game, 0, 0);
 
 		// Components
 		this.physics = new PhysicsBody(this);
 	}
 
-	setCrosshairPos(x,y) {
-		this.crosshairPos = {x,y};
+	setCrosshairPos(x, y) {
+		this.crosshairPos = { x, y };
 	}
 
 	update() {
-			this.physics.addImpulse(
-				(this.moving.right-this.moving.left)*this.speed,
-				(this.moving.up-this.moving.down/2)*this.speed+.2
-			);
+		this.physics.addImpulse(
+			(this.moving.right - this.moving.left) * this.speed,
+			(this.moving.up - this.moving.down / 2) * this.speed + .2
+		);
 		this.physics.update();
 		super.update();
 		this.crosshair.worldX = this.crosshairPos.x;
-		this.crosshair.worldY = this.worldY-this.crosshairPos.y +this.stage.canvas.height/2;
+		this.crosshair.worldY = this.worldY - this.crosshairPos.y + this.stage.canvas.height / 2;
 		this.crosshair.update();
 	}
 
 	getCrumb() {
-		switch(this.mode) {
+		switch (this.mode) {
 			case 0:
 				return {
-					pair:this.pair,
-					mode:this.mode,
-					x:this.worldX,
-					y:this.worldY,
+					name: this.name,
+					mode: this.mode,
+					x: this.worldX,
+					y: this.worldY,
 					moving: this.moving
 				}
 			case 1:
 				return {
-					pair:this.pair,
-					mode:this.mode,
-					crosshair:this.crosshairPos
+					name: this.name,
+					mode: this.mode,
+					crosshair: this.crosshairPos
 				}
-			}
+			default: 
+			 return {
+				 name: this.name,
+				 mode: this.mode,
+				 x: this.worldX,
+				 y: this.worldY,
+				 moving: this.moving,
+				 crosshair: this.crosshairPos
+			 }
+		}
 	}
 
 	updateCrumb(p) {
-		this.setPos(p.x,p.y);
-		this.moving = p.moving;
-		this.crosshairPos = this.crosshair;
+		p.mode && (this.mode = p.mode);
+		p.name && (this.name = p.name);
+		p.x && p.y && this.setPos(p.x, p.y);
+		p.moving && (this.moving = p.moving);
+		p.crosshair && (this.crosshairPos = p.crosshair);
 	}
 }
 
 class Coin extends GameObject {
-	constructor(game,x,y) {
-		super(game,x,y);
-		this.graphics.beginFill("Yellow").drawCircle(0,0,25);
+	constructor(game, x, y) {
+		super(game, x, y);
+		this.graphics.beginFill("Yellow").drawCircle(0, 0, 25);
 	}
 }
 
 class Game {
-	constructor(i,w,h) {
+	constructor(i, w, h) {
 		//Setup Stage
 		this.stage = new createjs.Stage(i);
 		this.stage.canvas.width = w;
@@ -154,18 +165,20 @@ class Game {
 
 		//Setup Game Loop
 		createjs.Ticker.setFPS(60);
-		createjs.Ticker.addEventListener("tick",this.update.bind(this));
+		createjs.Ticker.addEventListener("tick", this.update.bind(this));
 
 		//Setup Game Objects
-		this.player = new Player(this,w/2,0);
+		this.ships = {}
+		this.shipInfo = {};
+		/*this.player = new Ship(this, w / 2, 0);
 		this.coins = [];
 
 		//Setup Coins
-		this.coinPos = generateCoins(100,{x:w-200,y:h*50});
-		for (let i = 0; i < this.coinPos.length; i+=2) {
-			var coin = new Coin(this,this.coinPos[i]+100,this.coinPos[i+1]+w/2);
+		this.coinPos = generateCoins(100, { x: w - 200, y: h * 50 });
+		for (let i = 0; i < this.coinPos.length; i += 2) {
+			var coin = new Coin(this, this.coinPos[i] + 100, this.coinPos[i + 1] + w / 2);
 			this.coins.push(coin);
-		}
+		}*/
 
 		//Setup Events
 		window.onkeydown = this.inputDown.bind(this);
@@ -173,24 +186,67 @@ class Game {
 		this.stage.on("stagemousemove", this.mouseMove.bind(this));
 	}
 
+	login(ip) {
+		var socket = io(ip);
+		this.socket = socket;
+		//connect
+		socket.on('connect',function(){
+			console.log("Connected to server")
+		});
+		//joinShip
+		socket.on('joinShip',function(shipInfo) {
+			console.log("joinShip",shipInfo);
+			game.shipInfo.name = shipInfo.name;
+			game.shipInfo.mode = shipInfo.mode;
+
+			for(var i in shipInfo.ships) {
+				game.addShip(shipInfo.ships[i])
+			}
+		});
+		socket.on('addShip',game.addShip.bind(game));
+		socket.on('moveShip',function(m) {
+			console.log("moveShip",m);
+			game.ships[m.name].updateCrumb(m);
+		});
+		socket.on('moveCrosshair',function(c) {
+			console.log("moveCrosshair",c);
+			game.ships[c.name].updateCrumb(c);
+		});
+	}
+
+	addShip(s) {
+		if(game.ships[s.name]) return;
+		console.log("addShip",s);
+		var ship = new Ship(this, s.name);
+		ship.updateCrumb(s);
+		game.ships[ship.name] = ship;
+		if(ship.name == this.shipInfo.name) {
+			ship.mode = this.shipInfo.mode;
+			game.player=ship;
+		}
+	}
+
+
 	update(e) {
 		this.stage.canvas.width = window.innerWidth;
 		this.stage.canvas.height = window.innerHeight
 
-		this.player.update();
-		this.coins.forEach(coin=>{coin.update()})
+		//if(this.player) this.player.update();
+		if(this.player) Object.values(this.ships).forEach(ship => { ship.update() })
+		//this.coins.forEach(coin => { coin.update() })
 		this.stage.update();
 	}
 
 	mouseMove(e) {
-		if(this.player.mode!=1) return;
-		this.player.setCrosshairPos(e.stageX,e.stageY);
+		if (!this.player||this.player.mode != 1) return;
+		this.player.setCrosshairPos(e.stageX, e.stageY);
+		this.socket.emit("moveCrosshair",this.player.crosshairPos);
 	}
 
 	inputDown(e) {
-		if(this.player.mode!=0) return;
-		console.log("INPUT: "+e.keyCode)
-		switch(e.keyCode) {
+		if (!this.player||this.player.mode != 0) return;
+		console.log("INPUT: " + e.keyCode)
+		switch (e.keyCode) {
 			// Up (-X)
 			case 38: // UP ARROW
 			case 87: // W
@@ -211,48 +267,56 @@ class Game {
 			case 68: // D
 				this.player.moving.right = 1;
 				break;
-		}	
+		}
+		var {up,down,left,right} = this.player.moving;
+		if(!(up==0&&down==0&&left==0&&right==0)) this.socket.emit("moveShip",{x:this.player.x,y:this.player.y,moving:this.player.moving});
 	}
 
 	inputUp(e) {
-		console.log("INPUT: "+e.keyCode)
-		switch(e.keyCode) {
+		if(!this.player) return
+		console.log("INPUT: " + e.keyCode)
+		switch (e.keyCode) {
 			// Up (-X)
 			case 38: // UP ARROW
 			case 87: // W
 				this.player.moving.up = 0;
+				this.socket.emit("moveShip",{x:this.player.x,y:this.player.y,moving:this.player.moving});
 				break;
 			// Left (-Y)
 			case 37: // Left Arrow
 			case 65: // A
 				this.player.moving.left = 0;
+				this.socket.emit("moveShip",{x:this.player.x,y:this.player.y,moving:this.player.moving});
 				break;
 			// Down (+Y)
 			case 40: // Down Arrow
 			case 83: // S
 				this.player.moving.down = 0;
+				this.socket.emit("moveShip",{x:this.player.x,y:this.player.y,moving:this.player.moving});
 				break;
 			// Right (+X)
 			case 39: // Right Arrow
 			case 68: // D
 				this.player.moving.right = 0;
+				this.socket.emit("moveShip",{x:this.player.x,y:this.player.y,moving:this.player.moving});
 				break;
-		}	
-	}
-
-	worldToLocalPos(x,y) {
-		return {
-			x,
-			y:game.stage.canvas.height/2-(y-game.player.worldY)+game.player.physics.velocity.y
 		}
 	}
 
-	localToWorldPos(x,y) {
+	worldToLocalPos(x, y) {
 		return {
 			x,
-			y:game.player.worldY + (game.stage.canvas.height/2-y-game.player.physics.velocity.y)
+			y: game.stage.canvas.height / 2 - (y - game.player.worldY) + game.player.physics.velocity.y
+		}
+	}
+
+	localToWorldPos(x, y) {
+		return {
+			x,
+			y: game.player.worldY + (game.stage.canvas.height / 2 - y - game.player.physics.velocity.y)
 		}
 	}
 }
 
-game = new Game("viewport",window.innerWidth,window.innerHeight);
+game = new Game("viewport", window.innerWidth, window.innerHeight);
+game.login("http://localhost:3000")
---
+++ server.js
@@ -0,0 +1,143 @@
+"use strict";
+
+const socketIo = require('socket.io');
+var server;
+
+
+class Ship {
+	constructor(captain, cannon) {
+		this.x = 0;
+		this.y = 0;
+		this.moving = {
+			up: 0,
+			down: 0,
+			left: 0,
+			right: 0
+		};
+		this.crosshair = { x: 0, y: 0 };
+		this.captain = captain;
+		this.cannon = cannon;
+		captain.shipID = this.getID();
+		cannon.shipID = this.getID();
+
+		console.log("Ship Created: " + this.getID())
+	}
+
+	getID() {
+		return this.captain.socket.id + this.cannon.socket.id;
+	}
+
+	getCrumb() {
+		return {
+			name: this.getID(),
+			captain:this.captain.nickname,
+			cannon:this.cannon.nickname,
+			x: this.x,
+			y: this.y,
+			moving: this.moving,
+			crosshair: this.crosshair
+		}
+	}
+
+	join(room) {
+		this.room = room;
+		this.captain.socket.join(room);
+		this.cannon.socket.join(room);
+	}
+}
+
+class Client {
+	constructor(socket) {
+		this.socket = socket;
+		this.socket.emit("connect");
+
+		this.bindSocket("disconnect");
+		this.bindSocket("joinGame");
+		this.bindSocket("moveShip");
+		this.bindSocket("moveCrosshair");
+		console.log("Client Connected: " + socket.id);
+
+		this.room = "game";
+	}
+	
+	bindSocket(name) {
+		this.socket.on(name,this[name].bind(this));
+	}
+
+	disconnect() {
+		console.log("client disconnected:", this.socket.id);		
+	}
+	joinGame(nickname) {
+		console.log(this.socket.id,"joinGame",nickname)
+		this.nickname = nickname;
+		if (server.pending) {
+			var ship = new Ship(server.pending, this);
+			server.pending = undefined;
+			var shipName = ship.getID();
+			
+			this.emitAll('addShip',ship.getCrumb());
+			ship.join(this.room);
+
+			server.ships[shipName] = ship;
+			ship.captain.socket.emit('joinShip', {
+				name: shipName,
+				mode: 0,
+				ships: server.getShipCrumbs()
+			});
+			ship.cannon.socket.emit('joinShip', {
+				name: shipName,
+				mode: 1,
+				ships: server.getShipCrumbs()
+			});
+
+		} else {
+			server.pending = this;
+		}
+	}
+
+	moveShip(m) {
+		console.log(this.socket.id,"moveShip",m)
+		var {x,y,moving} = m;
+		if(!this.shipID) return;
+		server.ships[this.shipID].x = x;
+		server.ships[this.shipID].y = y;
+		server.ships[this.shipID].moving = moving;
+		this.emitAll('moveShip',{name:this.shipID,x,y,moving});
+	}
+	moveCrosshair(m) {
+		console.log(this.socket.id,"moveCrosshair",m)
+		if(!this.shipID) return;
+		server.ships[this.shipID].crosshair = m;
+		this.emitAll('moveCrosshair',{name:this.shipID,crosshair:m});
+	}
+
+	emitAll(...a) {
+		this.socket.broadcast.to(this.room).emit(...a);
+	}
+}
+
+class Server {
+	constructor(server) {
+		this.io = socketIo.listen(server);
+		this.clients = [];
+		this.ships = {};
+
+		this.io.on('connect', this.clientConnected.bind(this));
+		console.log("Created Server " + this.io.id);
+	}
+
+	clientConnected(socket) {
+		this.clients.push(new Client(socket));
+	}
+
+	getShipCrumbs() {
+		return Object.values(this.ships).map(s => s.getCrumb());
+	}
+}
+
+function main(httpServer) {
+	server = new Server(httpServer);
+	global.server = server
+}
+
+module.exports = main;
