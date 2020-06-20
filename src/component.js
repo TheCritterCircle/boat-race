61cb9f1561fbdf07a8bf9dc80f2ff3888f352598
Tumble <tumblegamer@gmail.com>
2020-06-20 15:48:11 +0100

    beep

--- src/component.js
+++
@@ -1,6 +0,0 @@
-class Component {
-	constructor(gameObject) {
-		this.gameObject = { worldX: 0, worldY: 0 };
-		gameObject && (this.gameObject = gameObject);
-	}
-}
--- src/gameobjects/coin.js
+++
@@ -1,8 +0,0 @@
-class Coin extends GameObject {
-	constructor(game, x, y) {
-		super(game, x, y);
-		this.coinGraphic = new createjs.Shape();
-		this.coinGraphic.graphics.beginFill("Yellow").drawCircle(0, 0, 25);
-		this.addChild(this.coinGraphic);
-	}
-}
